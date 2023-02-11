7f56210d4e5ea614d6b2c764002081dc53bb0194
Fuad Habib <justfuad@protonmail.com>
2023-02-12 02:11:57 +0300

    Add config file to load env variables

---
+++ user/core/config.py
@@ -0,0 +1,20 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    DATABASE_USERNAME: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_NAME: str
+
+    API_SECRET: str
+    ALGORITHM: str
+    TOKEN_EXPIRY: int
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
