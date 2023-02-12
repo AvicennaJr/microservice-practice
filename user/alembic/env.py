6b8fa5311f8c899c29d14ae1004bc73798b6cef9
Fuad Habib <justfuad@protonmail.com>
2023-02-12 15:53:08 +0300

    Fix imports

--- user/alembic/env.py
+++ user/alembic/env.py
@@ -3,8 +3,8 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-from core.config import settings
-from models import Base
+from app.core.config import settings
+from app.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
