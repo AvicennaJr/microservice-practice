046191ac94ba0142b85236ddb71b7f69fdb037f1
Fuad Habib <justfuad@protonmail.com>
2023-02-12 03:39:04 +0300

    remove models.Base.metadata since it will be handled by alembic

--- user/api/main.py
+++ user/api/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import models
 from db.session import engine
 
 from .endpoints import user
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.add_middleware(
