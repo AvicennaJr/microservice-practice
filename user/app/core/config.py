a63b85d4fafbea8d5379c8424f156c1856466aa6
Fuad Habib <justfuad@protonmail.com>
2023-02-13 13:52:01 +0300

    Add default values to config

--- user/app/core/config.py
+++ user/app/core/config.py
@@ -4,15 +4,24 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """Configure environmental variables"""
 
-    DATABASE_USERNAME: str
-    DATABASE_PASSWORD: str
-    DATABASE_HOST: str
-    DATABASE_PORT: str
-    DATABASE_NAME: str
+    # postgres database variables
+    DATABASE_USERNAME: str = "postgres"
+    DATABASE_PASSWORD: str = "postgres"
+    DATABASE_HOST: str = "localhost"
+    DATABASE_PORT: str = "5432"
+    DATABASE_NAME: str = "users"
 
+    # postgres test database variables
+    TEST_DATABASE_USERNAME: str = "postgres"
+    TEST_DATABASE_PASSWORD: str = "postgres"
+    TEST_DATABASE_HOST: str = "localhost"
+    TEST_DATABASE_PORT: str = "5432"
+    TEST_DATABASE_NAME: str = "users_test"
+
+    # JWT token variables
     API_SECRET: str
-    ALGORITHM: str
-    TOKEN_EXPIRY: int
+    ALGORITHM: str = "HS256"
+    TOKEN_EXPIRY: int = "60"
 
     class Config:
         env_file = ".env"
