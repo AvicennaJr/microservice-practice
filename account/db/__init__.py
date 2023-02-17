6107cc64e6c60bd01f6ce5ded4f92d40a32fb547
Fuad Habib <justfuad@protonmail.com>
2023-02-17 03:40:26 +0300

    Add db configuration

---
+++ account/db/__init__.py
@@ -0,0 +1,3 @@
+from .base import Base
+from .helpers import get_db
+from .session import engine, session
---
+++ account/db/session.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+session = SessionLocal()
